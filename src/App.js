85657643f8cbac8c63f3c3d037f592406fe2f83e
AlenaLarinaAL <alenalarina605@gmail.com>
2023-05-29 16:42:48 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Counter from "./components/Counter";
+import Dropdown from "./components/Dropdown";
+import ColorPicker from "./components/ColorPicker";
+import TodoList from "./components/TodoList";
+import initialTodos from "./todos.json";
+
+const colorPickerOptions = [
+  { label: "red", color: "#F44336" },
+  { label: "green", color: "#4CAF50" },
+  { label: "blue", color: "#2196F3" },
+  { label: "grey", color: "#607D8B" },
+  { label: "pink", color: "#E91E63" },
+  { label: "indigo", color: "#3F51B5" },
+];
+
+class App extends Component {
+  state = {
+    todos: initialTodos,
+  };
+
+  deleteTodo = (todoId) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== todoId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <>
+        <h1>Состояние компонентов</h1>
+        <Counter initialValue={0} />
+        <Dropdown />
+        <ColorPicker options={colorPickerOptions} />
+
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
+
+export default App;
