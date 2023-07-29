5ea015da36120e3ca023f6636c6baccbf540df64
AlenaLarinaAL <alenalarina605@gmail.com>
2023-07-29 15:32:03 +0000

    add

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./components/App/App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AuthProvider from './contexts/auth/Provider';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
-
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
-
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
