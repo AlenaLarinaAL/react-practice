52c833c8e8b972254f41c7d963e76ef89577986e
AlenaLarinaAL <alenalarina605@gmail.com>
2023-07-30 18:16:21 +0000

    add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AppBar from "./components/AppBar/AppBar";
 import SignupForm from "./components/SignupForm/SignupForm";
 import ColorPicker from "./components/ColorPicker/ColorPicker";
-import Counter from "./components/Counter/Counter";
+import { Counter } from "./components/Counter/Counter";
 import Clock from "./components/Clock/Clock";
 import PokemonView from "./views/PokemonView";
 
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,45 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from "./Counter.module.css";
 
-// export default function Counter() {
-//   const [counterA, setCounterA] = useState(0);
-//   const [counterB, setCounterB] = useState(0);
-
-//   const handleCounterAIncrement = () => {
-//     setCounterA(state => state + 1);
-//   };
-
-//   const handleCounterBIncrement = () => {
-//     setCounterB(state => state + 1);
-//   };
-
-//   useEffect(() => {
-//     const totalClicks = counterA + counterB;
-//     document.title = `Всего кликнули ${totalClicks} раз`;
-//   }, [counterA, counterB]);
-
-//   return (
-//     <>
-//       <button
-//         className={styles.btn}
-//         type="button"
-//         onClick={handleCounterAIncrement}
-//       >
-//         Кликнули counterA {counterA} раз
-//       </button>
-
-//       <button
-//         className={styles.btn}
-//         type="button"
-//         onClick={handleCounterBIncrement}
-//       >
-//         Кликнули counterB {counterB} раз
-//       </button>
-//     </>
-//   );
-// }
-
-export default function Counter() {
+export const Counter = () => {
   const [counterA, setCounterA] = useState(0);
   const [counterB, setCounterB] = useState(0);
 
@@ -53,7 +15,7 @@ export default function Counter() {
 
   useEffect(() => {
     const totalClicks = counterA + counterB;
-    document.title = `we clicked ${totalClicks} times!`;
+    document.title = `Всего кликнули ${totalClicks} раз`;
   }, [counterA, counterB]);
 
   return (
@@ -75,7 +37,7 @@ export default function Counter() {
       </button>
     </>
   );
-}
+};
 
 // class OldCounter extends Component {
 //   state = {
